42b1c4370d9ef2ec84faea8ef7200dddf563024e
kazuki1214 <changex02atrex@gmail.com>
2022-02-25 22:39:11 +0000

    [Add]ユーザーホーム画面、新規作成画面、ログイン画面作成

--- app/controllers/end_user/homes_controller.rb
+++ app/controllers/end_user/homes_controller.rb
@@ -1,7 +1,8 @@
 class EndUser::HomesController < ApplicationController
 
   def top
-    @ranks_hobby = Hobby.find(FavoriteHobby.group(:hobby_id).order('count(hobby_id) desc').limit(3).pluck(:hobby_id))
+    @ranking_hobbies = Hobby.find(FavoriteHobby.group(:hobby_id).order('count(hobby_id) desc').limit(3).pluck(:hobby_id))
+    @ranking_posts = Post.find(FavoritePosts.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
 end
