1f3ff1ce7a6b40f3344efb1b66d4897a76d9b0aa
kazuki1214 <changex02atrex@gmail.com>
2022-02-25 14:05:42 +0000

    [Add]管理者ユーザー投稿詳細画面作成

--- app/controllers/admin/post_histories_controller.rb
+++ app/controllers/admin/post_histories_controller.rb
@@ -5,8 +5,11 @@ class Admin::PostHistoriesController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    comments = @post.comments.includes(:end_user).order(created_at: :desc)
+    @comments_first = comments.first(3)
+    @comments_offset = comments.offset(3)
   end
-  
+
   def destroy
     @post = Posthistrory.find(params[:id]).destroy
   end
