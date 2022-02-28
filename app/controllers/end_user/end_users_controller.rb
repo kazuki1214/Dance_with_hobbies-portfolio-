ec30465ddfb4392eed9684995ac1d0410394bea1
kazuki1214 <changex02atrex@gmail.com>
2022-02-28 09:52:20 +0000

    [Add]ユーザー編集画面作成

--- app/controllers/end_user/end_users_controller.rb
+++ app/controllers/end_user/end_users_controller.rb
@@ -1,16 +1,29 @@
 class EndUser::EndUsersController < ApplicationController
-  
+
   def show
     @user = EndUser.find(params[:id])
   end
-  
+
   def edit
+    @user = current_end_user
   end
-  
+
   def update
+    user = current_end_user
+    user.update(user_params)
+    redirect_to end_user_path(user)
   end
-  
+
   def withdraw
+    user = current_end_user
+    user.update(is_deleted: true)
+    redirect_to root_path
   end
-  
+
+  private
+
+  def user_params
+    params.require(:end_user).permit(:name, :email, :user_commet, :user_image )
+  end
+
 end
