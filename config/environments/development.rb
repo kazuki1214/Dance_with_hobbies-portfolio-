f1d911f34f30c1deace63994362c3a08b0842218
kazuki1214 <changex02atrex@gmail.com>
2022-04-29 13:30:34 +0000

    [Update]お問合せ機能本番環境設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,15 +62,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  user_name = ENV['SMTP_USERNAME']
-  pass = ENV['SMTP_PASSWORD']
 
   config.action_mailer.smtp_settings = {
     port: 587,
     address: 'smtp.gmail.com',
     domain: 'smtp.gmail.com',
-    user_name: user_name,
-    password: pass,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
     enable_starttls_auto: true,
     authentication: :plain
   }
